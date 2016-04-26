c3b528013d67094006a5b781f493b183be932c02
Rati Wannapanop <rati.wannapanop@gmail.com>
2016-04-26 11:46:52 +0700

    initial commit

---
+++ app/Http/routes.php
@@ -0,0 +1,38 @@
+<?php
+
+Route::get('/api/users', function() {
+$request = request();
+
+// handle sort option
+if (request()->has('sort')) {
+    list($sortCol, $sortDir) = explode('|', request()->sort);
+    $query = App\User::orderBy($sortCol, $sortDir);
+} else {
+    $query = App\User::orderBy('id', 'asc');
+}
+
+if ($request->exists('filter')) {
+    $query->where(function($q) use($request) {
+        $value = "%{$request->filter}%";
+        $q->where('name', 'like', $value)
+            ->orWhere('nickname', 'like', $value)
+            ->orWhere('email', 'like', $value);
+    });
+}
+
+$perPage = request()->has('per_page') ? (int) request()->per_page : null;
+
+// The headers 'Access-Control-Allow-Origin' and 'Access-Control-Allow-Methods'
+// are to allow you to call this from any domain (see CORS for more info).
+// This is for local testing only. You should not do this in production server,
+// unless you know what it means.
+return response()->json(
+        $query->paginate($perPage)
+    )
+    ->header('Access-Control-Allow-Origin', '*')
+    ->header('Access-Control-Allow-Methods', 'GET');
+});
+
+Route::get('/', function() {
+    return redirect('https://github.com/ratiw/vue-table');
+});
---
+++ database/factories/ModelFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+$factory->define(App\User::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'nickname' => $faker->word,
+        'email' => $faker->safeEmail,
+        'password' => bcrypt(str_random(10)),
+        'remember_token' => str_random(10),
+        'birthdate' => $faker->dateTimeBetween('-30 years', 'now'),
+        'gender' => $faker->randomElement(['M', 'F']),
+        'group_id' => $faker->randomElement([1, 2, 3, 4, 5])
+    ];
+});
