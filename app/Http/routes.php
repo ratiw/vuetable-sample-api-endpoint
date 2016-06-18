d976dd6744126f3abbf278fba03c722e1876245a
Rati Wannapanop <rati.wannapanop@gmail.com>
2016-06-18 22:13:40 +0700

    support multisort columns, appends pagination data

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,36 +1,49 @@
 <?php
 
 Route::get('/api/users', function() {
-$request = request();
+    $request = request();
 
-// handle sort option
-if (request()->has('sort')) {
-    list($sortCol, $sortDir) = explode('|', request()->sort);
-    $query = App\User::orderBy($sortCol, $sortDir);
-} else {
-    $query = App\User::orderBy('id', 'asc');
-}
+    $query = app(App\User::class)->newQuery();
 
-if ($request->exists('filter')) {
-    $query->where(function($q) use($request) {
-        $value = "%{$request->filter}%";
-        $q->where('name', 'like', $value)
-            ->orWhere('nickname', 'like', $value)
-            ->orWhere('email', 'like', $value);
-    });
-}
+    // handle sort option
+    if (request()->has('sort')) {
+        // handle multisort
+        $sorts = explode(',', request()->sort);
+        foreach ($sorts as $sort) {
+            list($sortCol, $sortDir) = explode('|', $sort);
+            $query = $query->orderBy($sortCol, $sortDir);
+        }
+    } else {
+        $query = $query->orderBy('id', 'asc');
+    }
 
-$perPage = request()->has('per_page') ? (int) request()->per_page : null;
+    if ($request->exists('filter')) {
+        $query->where(function($q) use($request) {
+            $value = "%{$request->filter}%";
+            $q->where('name', 'like', $value)
+                ->orWhere('nickname', 'like', $value)
+                ->orWhere('email', 'like', $value);
+        });
+    }
 
-// The headers 'Access-Control-Allow-Origin' and 'Access-Control-Allow-Methods'
-// are to allow you to call this from any domain (see CORS for more info).
-// This is for local testing only. You should not do this in production server,
-// unless you know what it means.
-return response()->json(
-        $query->paginate($perPage)
-    )
-    ->header('Access-Control-Allow-Origin', '*')
-    ->header('Access-Control-Allow-Methods', 'GET');
+    $perPage = request()->has('per_page') ? (int) request()->per_page : null;
+
+    $pagination = $query->paginate($perPage);
+    $pagination->appends([
+        'sort' => request()->sort,
+        'filter' => request()->filter,
+        'per_page' => request()->per_page
+    ]);
+
+    // The headers 'Access-Control-Allow-Origin' and 'Access-Control-Allow-Methods'
+    // are to allow you to call this from any domain (see CORS for more info).
+    // This is for local testing only. You should not do this in production server,
+    // unless you know what it means.
+    return response()->json(
+            $pagination
+        )
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', 'GET');
 });
 
 Route::get('browserify/bootstrap', function() {
